35f0ddc38e8112efcef1eb9b98366e6ebbe735f5
Aditya Yuga <adityayuga@Adityas-MacBook-Air.local>
2023-10-02 19:23:55 +0700

    Add stop consumer

--- streamer/stop.go
+++ streamer/stop.go
@@ -1,29 +1,33 @@
 package streamer
 
 import (
-	"sync"
-
 	"github.com/nsqsink/sink/consumer"
 )
 
 // Stop to stop all consumer handler in the consumer
 // using go routine to make it faster
 func (m *NSQModule) Stop() error {
-	var (
-		err error
-		wg  sync.WaitGroup
-		mux sync.Mutex
-	)
-
-	// add wait group based on list of consumer registered
-	wg.Add(len(m.consumers))
+	errChan := make(chan error)
 
 	for _, c := range m.consumers {
 		go func(nsqConsumer *consumer.Consumer) {
-			defer wg.Done()
+			var err error
+			defer func() {
+				errChan <- err
+			}()
 
-		}(c)
+			err = c.Stop()
+		}(&c)
 	}
 
-	return nil
+	close(errChan)
+
+	var err error
+	for tempErr := range errChan {
+		if tempErr != nil {
+			err = tempErr
+		}
+	}
+
+	return err
 }
