d411717a10db2e065a8615a0ed07b8d035d5a5d0
Muhammad Zaki Al-Afrani <zaki.afrani@gmail.com>
2024-03-24 13:38:59 +0700

    add docker-compose nsq services (#11)
    * add docker-compose nsq services
    
    * fix tests http

--- test/http/test_http_server.go
+++ test/http/test_http_server.go
@@ -1,10 +1,21 @@
 package http
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/retrieve-post-json", getRoot)
 	http.HandleFunc("/retrieve-post-json", getHello)
 
-	err := http.ListenAndServe(":8007", nil)
+	http.ListenAndServe(":8007", nil)
+}
+
+func getRoot(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "root\n")
+}
+
+func getHello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
 }
